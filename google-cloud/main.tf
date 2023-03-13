eaee69e2310f492e804d8633455f1160d8dd332f
Hilmi Afifi <38133643+hilmi-afifi@users.noreply.github.com>
2023-03-14 03:51:16 +0700

    Create

---
+++ google-cloud/main.tf
@@ -0,0 +1,11 @@
+# set provider google cloud on jakarta
+provider "google" {
+    project = "project-id"
+    region = "asia-southeast2"
+    credentials = var.google_credentials
+}
+
+# define variable for google credentials
+variable "google_credentials" {
+  description = "google credentials"
+}
