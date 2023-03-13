797118db474ebb68f2fe54f53591b7de55eb6082
Hilmi Afifi <38133643+hilmi-afifi@users.noreply.github.com>
2023-03-14 03:05:09 +0700

    Create

---
+++ install_googlecloudsdk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#  install Google Cloud SDK on master-vm linux ubuntu
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list;
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -;
+sudo apt update && sudo apt install google-cloud-sdk -y;
+gcloud version
