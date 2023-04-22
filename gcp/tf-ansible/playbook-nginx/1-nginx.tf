969e93f676e9d6051cd1bb1fe5eb757b5509ea6d
Hilmi Afifi <38133643+xhilmi@users.noreply.github.com>
2023-04-22 15:12:45 +0700

    Create 1-nginx.tf

---
+++ gcp/tf-ansible/playbook-nginx/1-nginx.tf
@@ -0,0 +1,70 @@
+locals {
+  project_id       = "first-renderer-376510"
+  network          = "default"
+  image            = "ubuntu-2204-jammy-v20230421"
+  ssh_user         = "ansible"
+  private_key_path = "~/.ssh/ansible"
+}
+
+provider "google" {
+  project = local.project_id
+  region  = "us-west1"
+}
+
+resource "google_service_account" "nginx" {
+  account_id = "nginx-demo"
+}
+
+resource "google_compute_firewall" "web" {
+  name    = "web-access"
+  network = local.network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges           = ["0.0.0.0/0"]
+  target_service_accounts = [google_service_account.nginx.email]
+}
+
+resource "google_compute_instance" "nginx" {
+  name         = "nginx"
+  machine_type = "e2-medium"
+  zone         = "us-west1-a"
+
+  boot_disk {
+    initialize_params {
+      image = local.image
+    }
+  }
+
+  network_interface {
+    network = local.network
+    access_config {}
+  }
+
+  service_account {
+    email  = google_service_account.nginx.email
+    scopes = ["cloud-platform"]
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = google_compute_instance.nginx.network_interface.0.access_config.0.nat_ip
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${google_compute_instance.nginx.network_interface.0.access_config.0.nat_ip}, --private-key ${local.private_key_path} nginx.yaml"
+  }
+}
+
+output "nginx_ip" {
+  value = google_compute_instance.nginx.network_interface.0.access_config.0.nat_ip
+}
