2da9f6f7c10ed752e1d8e0df455c7d45a1b6f129
xhilmi <hilmiafifi12@gmail.com>
2023-08-09 07:20:06 +0700

    feat (xhilmi): add something

--- gcp/tf-origin/instance/v4/main.tf
+++ gcp/tf-origin/instance/v4/main.tf
@@ -1,6 +1,6 @@
 # define provider
 provider "google" {
-  project     = "project" # <PROJECT_ID>
+  project     = "sisuper-390014" # <PROJECT_ID>
   # region      = "asia-southeast2"
   credentials = var.google_credentials
 }
@@ -29,12 +29,12 @@ resource "google_compute_instance" "k8s_master_node" {
 
 ##### K8S WORKER NODE #####
 # create instance worker node
-resource "google_compute_instance" "k8s_worker_node_a" {
+resource "google_compute_instance" "k8s_worker_node" {
   # create multiple instance using number 
-  count        = 1
-  name         = "k8s-worker-a-${count.index + 1}"
+  count        = 2
+  name         = "k8s-worker-${count.index + 1}"
   machine_type  = var.machine_type[0].e2-medium
-  zone          = var.machine_zone[0].us-west1
+  zone          = var.machine_zone[0].us-central1
   boot_disk {
     initialize_params {
       image   = var.boot_disk[1].image
@@ -53,48 +53,48 @@ resource "google_compute_instance" "k8s_worker_node_a" {
 
 ##### K8S WORKER NODE #####
 #create instance worker node
-resource "google_compute_instance" "k8s_worker_node_b" {
-  # create multiple instance using number 
-  count        = 1
-  name         = "k8s-worker-b-${count.index + 1}"
-  machine_type  = var.machine_type[0].e2-medium
-  zone          = var.machine_zone[0].northamerica-northeast1
-  boot_disk {
-    initialize_params {
-      image   = var.boot_disk[1].image
-      size    = var.boot_disk[1].size
-      type    = var.boot_disk[1].type
-    }
-  }
-  network_interface {
-    network   = var.network_interface
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-  metadata_startup_script = var.startup_script[0].worker
-}
+# resource "google_compute_instance" "k8s_worker_node_b" {
+#   # create multiple instance using number 
+#   count        = 1
+#   name         = "k8s-worker-b-${count.index + 1}"
+#   machine_type  = var.machine_type[0].e2-medium
+#   zone          = var.machine_zone[0].us-central1
+#   boot_disk {
+#     initialize_params {
+#       image   = var.boot_disk[1].image
+#       size    = var.boot_disk[1].size
+#       type    = var.boot_disk[1].type
+#     }
+#   }
+#   network_interface {
+#     network   = var.network_interface
+#     access_config {
+#       // Ephemeral public IP
+#     }
+#   }
+#   metadata_startup_script = var.startup_script[0].worker
+# }
 
 # #### K8S WORKER NODE #####
 # create instance worker node
-resource "google_compute_instance" "k8s_worker_node_c" {
-  # create multiple instance using number 
-  count        = 1
-  name         = "k8s-worker-c-${count.index + 1}"
-  machine_type  = var.machine_type[0].e2-medium
-  zone          = var.machine_zone[0].northamerica-northeast2
-  boot_disk {
-    initialize_params {
-      image   = var.boot_disk[1].image
-      size    = var.boot_disk[1].size
-      type    = var.boot_disk[1].type
-    }
-  }
-  network_interface {
-    network   = var.network_interface
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-  metadata_startup_script = var.startup_script[0].worker
-}
+# resource "google_compute_instance" "k8s_worker_node_c" {
+#   # create multiple instance using number 
+#   count        = 1
+#   name         = "k8s-worker-c-${count.index + 1}"
+#   machine_type  = var.machine_type[0].e2-medium
+#   zone          = var.machine_zone[0].northamerica-northeast2
+#   boot_disk {
+#     initialize_params {
+#       image   = var.boot_disk[1].image
+#       size    = var.boot_disk[1].size
+#       type    = var.boot_disk[1].type
+#     }
+#   }
+#   network_interface {
+#     network   = var.network_interface
+#     access_config {
+#       // Ephemeral public IP
+#     }
+#   }
+#   metadata_startup_script = var.startup_script[0].worker
+# }
